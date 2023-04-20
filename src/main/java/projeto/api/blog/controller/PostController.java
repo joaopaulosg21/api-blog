bb2248e9be7aab68f322a683c5f986343fe0faf8
JoãoPaulo <joaopaulosg17@gmail.com>
2023-04-20 10:48:44 -0300

    Adicionado metodo update no PostController

--- src/main/java/projeto/api/blog/controller/PostController.java
+++ src/main/java/projeto/api/blog/controller/PostController.java
@@ -8,7 +8,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,5 +36,10 @@ public class PostController {
     @GetMapping("/all")
     public ResponseEntity<List<Post>> findAllPosts() {
         return ResponseEntity.ok(postService.findAllPosts());
+    }
+
+    @PutMapping("/update/{postId}")
+    public ResponseEntity<DefaultResponse> updatePost(@AuthenticationPrincipal UserDTO userDTO, @Valid @RequestBody Post post, @PathVariable long postId) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(postService.updatePost(post, userDTO, postId));
     }
 }
